6f05f72835f64778d5c29a87cf6e17ada2753d21
yangshunfeng <972536780@qq.com>
2017-06-27 16:12:32 +0800

    启动类

--- springboot/src/main/java/com/yangsf/SpringbootApplication.java
+++ springboot/src/main/java/com/yangsf/SpringbootApplication.java
@@ -2,11 +2,18 @@ package com.yangsf;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class SpringbootApplication {
+    @RequestMapping("/")
+    public String index() {
+        return "hello world!";
+    }
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringbootApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootApplication.class, args);
+    }
 }

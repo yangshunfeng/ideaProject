a1c6e57c0ac62c4bc1d1ed212c84fe45159a4577
yangshunfeng <972536780@qq.com>
2017-09-02 17:47:16 +0800

    默认的

---
+++ program/src/main/java/acm/Main.java
@@ -0,0 +1,10 @@
+package acm;
+
+/**
+ * Created by 972536780 on 2017/9/2.
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
